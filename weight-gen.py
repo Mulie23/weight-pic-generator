7c426d08247867189be2828bf01338a32b1a8744
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-09-18 19:25:06 +0800

    update

--- weight-gen.py
+++ weight-gen.py
@@ -8,13 +8,12 @@ def generatePic(weight):
     d = ImageDraw.Draw(img)
     fnt = ImageFont.truetype("arial.ttf", 400)
     w, h = d.textsize(weight_str,font=fnt)
-    print(w,h)
-    d.text(((W-w)/2,(H-h)/2), weight_str, font=fnt, fill=(255,255,255))
+    d.text(((W-w)/2,380), weight_str, font=fnt, fill=(255,255,255))
     
-    img.save('D:/weight-pic-generator/weight_pic.png')
+    img.save('D:/weight_pic.png')
 
 def setWallpaper():
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, 'D:/weight-pic-generator/weight_pic.png', 0)  # set wallpaper
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, 'D:/weight_pic.png', 0)  # set wallpaper
 
 input_weight = input("Input your weight:")
 generatePic(input_weight)
